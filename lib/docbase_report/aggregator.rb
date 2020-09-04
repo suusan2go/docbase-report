ace4da8776099ff6c1aa9dabef13084ad85f5276
suusan2go <ksuzuki180@gmail.com>
2020-09-04 10:58:28 +0900

    implement aggregation code

--- lib/docbase_report/aggregator.rb
+++ lib/docbase_report/aggregator.rb
@@ -5,42 +5,49 @@ require 'docbase'
 module DocBaseReport
   class Aggregator
     def initialize(client = nil)
-      @client = client || DocBase::Client.new(access_token: "", team: "") # TODO: load from environment variable
+      @client = client || DocBase::Client.new(access_token: ENV["DOCBASE_ACCESS_TOKEN"], team: ENV["DOCBASE_TEAM"])
     end
 
     def fetch_posts(from, to)
       posts = []
+      page = 1
       begin
-        new_posts = client.posts(q: "created_at:#{from}~#{to}", page: 1, per_page: 100);
+        res = @client.posts(q: "created_at:#{from}~#{to}", page: page, per_page: 100);
+        if res.status != 200
+          raise "request failed status:#{res.status} body: #{res.body}"
+        end
+        new_posts = res.body["posts"]
         posts.concat(new_posts)
+        page += 1
       end while new_posts.length == 100
       Summary.new(posts)
     end
   end
 
   class Summary
+    attr_reader :posts
     def initialize(posts)
       @posts = posts
     end
 
     def good_jobs_ranking
-      posts.sort_by { |post| post[:stars_count] }
+      @posts.sort_by { |post| post["good_jobs_count"] }.reverse
     end
 
     def stared_ranking
-      posts.sort_by { |post| post[:started_count] }
+      @posts.sort_by { |post| post["stars_count"] }.reverse
     end
 
     def commented_ranking
-      posts.sort_by { |post| post[:comments].length }
+      @posts.sort_by { |post| post["comments"].length }.reverse
     end
 
     def number_of_posts_per_day
-      posts.group_by { |post| post[:created_at].to_date }
+      @posts.group_by { |post| post["created_at"].to_date }.reverse
     end
 
     def number_of_posts_per_month
-      posts.group_by { |post| post[:created_at].to_date.beginning_of_month }
+      @posts.group_by { |post| post["created_at"].to_date.beginning_of_month }.reverse
     end
   end
 end
